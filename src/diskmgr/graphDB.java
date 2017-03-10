eb2c09c70767480a049d2b1e0df590e02d1bafdb
elanmarkov <elanmarkov@asu.edu>
2017-03-10 12:00:53 -0700

    Added some features to Task 8 (still needs integration)

--- src/diskmgr/graphDB.java
+++ src/diskmgr/graphDB.java
@@ -12,26 +12,65 @@ import global.*;
 import btree.*;
 
 public class graphDB extends DB {
-	//NodeHeapFile nodes;
-	//EdgeHeapFile edges;
-	BT nodeLabels;
-	//ZT nodeDesc;
-	BT edgeLabels;
-	BT edgeWeights;
+	static int numGraphDB = 0;
+	NodeHeapFile nodes;
+	EdgeHeapFile edges;
+	BTreeFile nodeLabels;
+	ZCurve nodeDesc;
+	BTreeFile edgeLabels;
+	BTreeFile edgeWeights;
+	int type; // Clustering/indexing strategy descriptor
+	final int KEY_SIZE = 80;
 	
-	public graphDB() {
+	/**
+	Default constructor. Type specifies parameters for keytype and delete_fashion
+	0 for integer key, naive delete
+	1 for integer key, full delete
+	2 for string key, naive delete
+	3 for string key, full delete
+	*/
+	public graphDB(int type) {
 		super();
+		this.type = type; 
+		String fileName = "GraphDB" + numGraphDB;
+		numGraphDB++;
+		switch(type) {
+		case 0:	
+		// integer key, naive delete
+			nodeLabels = new BTreeFile(filename + "NODELABEL", 0, KEY_SIZE, 0);
+			edgeLabels = new BTreeFile(filename + "EDGELABEL", 0, KEY_SIZE, 0);
+			edgeWeights = new BTreeFile(filename + "EDGEWEIGHT", 0, KEY_SIZE, 0);
+			break;
+		case 1:
+		// integer key, full delete
+			nodeLabels = new BTreeFile(filename + "NODELABEL", 0, KEY_SIZE, 1);
+			edgeLabels = new BTreeFile(filename + "EDGELABEL", 0, KEY_SIZE, 1);
+			edgeWeights = new BTreeFile(filename + "EDGEWEIGHT", 0, KEY_SIZE, 1);
+			break;
+		case 2:
+		// string key, naive delete
+			nodeLabels = new BTreeFile(filename + "NODELABEL", 1, KEY_SIZE, 0);
+			edgeLabels = new BTreeFile(filename + "EDGELABEL", 1, KEY_SIZE, 0);
+			edgeWeights = new BTreeFile(filename + "EDGEWEIGHT", 1, KEY_SIZE, 0);
+			break;
+		case 3:
+		// string key, full delete
+			nodeLabels = new BTreeFile(filename + "NODELABEL", 1, KEY_SIZE, 1);
+			edgeLabels = new BTreeFile(filename + "EDGELABEL", 1, KEY_SIZE, 1);
+			edgeWeights = new BTreeFile(filename + "EDGEWEIGHT", 1, KEY_SIZE, 1);
+			break;
+		default:
+			throw new ClassCastException("No such type for keytype/delete_fashion (in GraphDB.java)");
+		}
 		// initialize each method; to be implemented when combined
 	}
 
 	public int getNodeCnt() {
-		return 0;
-		//return nodes.getNodeCnt();
+		return nodes.getNodeCnt();
 	}
 	
 	public int getEdgeCnt() {
-		return 0;
-		//return edges.getEdgeCnt();
+		return edges.getEdgeCnt();
 	}
 	
 	public int getSourceCnt() {

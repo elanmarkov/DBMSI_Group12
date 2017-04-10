3757fc31fd75e7428ee47aa2de824cbc1ba7b079
elanmarkov <elanmarkov@asu.edu>
2017-04-09 18:25:51 -0700

    Added IndexNestedJoin with stubs

---
+++ src/iterator/IndexNestedJoin.java
@@ -0,0 +1,26 @@
+/** Index Nested Loop Join by Elan Markov
+CSE 510 Phase 3 Group 12
+Task 1 */
+package iterator;
+
+import global.AttrOperator;
+import global.AttrType;
+import global.Descriptor;
+import global.TupleOrder;
+import heap.Tuple;
+
+public class IndexNestedJoin {
+	// Stubs for now
+	public static void sourceEdgeJoin (CondExpr edgeCond) {
+		// Node JOIN (source, edge_condition) Edge
+	}
+	public static void destEdgeJoin (CondExpr edgeCond) {
+		// Node JOIN (dest, edge_condition) Edge
+	}
+	public static void edgeSourceJoin (CondExpr nodeCond) {
+		// Edge JOIN (source, node_condition) Node
+	}
+	public static void edgeSourceJoin (CondExpr nodeCond) {
+		// Edge JOIN (dest, node_condition) Node
+	}
+}
